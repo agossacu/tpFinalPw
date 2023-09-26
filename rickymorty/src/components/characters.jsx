9c8d029039aeb39b7135014a30ce05b387e84411
agossacu <agostinaacu28@gmail.com>
2023-09-26 16:26:58 -0300

    Cambios y funciona

--- rickymorty/src/components/characters.jsx
+++ rickymorty/src/components/characters.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import "../characters.css";
+import ".././characters.css";
+
 export function Characters() {
   const [characters, setCharacters] = useState([]);
 
@@ -11,15 +12,22 @@ export function Characters() {
 
   return (
     <div>
-      <ul className="c">
-        {characters.map((item, index) => (
-          <li key={index}>
-            <h3 className="colorNaranja"> {item.name} </h3>
-            <p>{item.status}</p>
-            <img src={item.image} />
-          </li>
-        ))}
-      </ul>
+      hola
+      <div id="fondoCharacter">
+        <ul className="container">
+          {characters.map((item, index) => (
+            <li key={index}>
+              <img src={item.image} />
+              <h3 className="colorBlanco"> {item.name} </h3>
+              <h4>{item.status}</h4>
+              <p>Last known location:</p>
+              <h4></h4>
+              <p>First seen in:</p>
+              <h4></h4>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
