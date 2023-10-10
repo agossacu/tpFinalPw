f30080d075e04da9e4b1560489d044554ce60fba
agossacu <agostinaacu28@gmail.com>
2023-10-10 17:04:01 -0300

    tengo que centrar la fotoooooooo

--- rickymorty/src/components/characters.jsx
+++ rickymorty/src/components/characters.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ".././characters.css";
 import { useCharacter } from "../hooks/useCharacter";
+import { Svg } from "./svg";
 
 export function Characters() {
   const { characters, getAllCharacters } = useCharacter();
@@ -12,20 +13,26 @@ export function Characters() {
     <>
       <div>
         <ul className="header">
-          <li>Docs</li>
-          <li>About</li>
-          <li>Support Us</li>
+          <li className="hover separar">Docs</li>
+          <li className="hover separar">About</li>
+          <li className="support hover-fondo">SUPPORT US</li>
         </ul>
       </div>
-      <h1> The Rick and Morty API</h1>
+      <div>
+        <h1 className="absol"> The Rick and Morty API</h1>
+        <Svg className="relativ" />
+      </div>
       <div>
         <ul className="lista">
           {characters.map((item, index) => (
             <li className="fondoCharacter colorGris" key={index}>
               <img src={item.image} />
               <div className="fondoTexto">
-                <h3 className="colorBlanco"> {item.name} </h3>
+                <h2 className="colorBlanco hover"> {item.name} </h2>
                 <h4 className="colorBlanco">{item.status}</h4>
+                <span>
+                  <span class="status__icon"></span>
+                </span>
                 <p className="gris">Last known location:</p>
                 <h4 className="colorBlanco hover">{item.location.name}</h4>
                 <p className="gris">First seen in:</p>
