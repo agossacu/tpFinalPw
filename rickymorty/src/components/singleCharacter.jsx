5709d92d98bff4feb8325e4e6d9235af7ebdb955
agossacu <agostinaacu28@gmail.com>
2023-10-31 17:01:06 -0300

    ya hice hasta el punto 4

--- rickymorty/src/components/singleCharacter.jsx
+++ rickymorty/src/components/singleCharacter.jsx
@@ -1,6 +1,10 @@
+import { useCharacter } from "../hooks/useCharacter";
+
 export function SingleCharacter(props) {
   const { personaje } = props;
-  const { image, name, status, species, location, episode } = personaje;
+  const { image, name, status, species, location, episode, id } = personaje;
+
+  const { getAllEpisodes } = useCharacter();
 
   const getStatusColor = (status) => {
     if (status === "Alive") {
@@ -12,8 +16,15 @@ export function SingleCharacter(props) {
       return "depa";
     }
   };
+  let epiNum = [];
+  const handleClick = () => {
+    episode.map((episodio) => epiNum.push(episodio.slice(40)));
+
+    getAllEpisodes(epiNum);
+  };
+
   return (
-    <li className="fondoCharacter colorGris" key={index}>
+    <li className="fondoCharacter colorGris" key={id}>
       <img src={image} />
       <div className="fondoTexto">
         <h2 className="colorBlanco hover"> {name} </h2>
@@ -24,8 +35,8 @@ export function SingleCharacter(props) {
 
         <p className="gris">Last known location:</p>
         <h4 className="colorBlanco hover">{location.name}</h4>
-        <p className="gris">First seen in:</p>
-        <h4 className="colorBlanco hover">{location.name}</h4>
+        <button onClick={handleClick}>Episodios</button>
+        <p className="colorBlanco hover"> Episodes {episode}</p>
       </div>
     </li>
   );
--- rickymorty/src/hooks/useCharacter.js
+++ rickymorty/src/hooks/useCharacter.js
@@ -1,14 +1,22 @@
 import { useState } from "react";
 export function useCharacter() {
   const [characters, setCharacters] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+
   const getAllCharacters = () => {
     return fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
       .then((data) => setCharacters(data.results));
   };
+  const getAllEpisodes = (episodes) => {
+    return fetch(`https://rickandmortyapi.com/api/episode/${episodes}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data.results.name));
+  };
 
   return {
     characters,
     getAllCharacters,
+    getAllEpisodes,
   };
 }
